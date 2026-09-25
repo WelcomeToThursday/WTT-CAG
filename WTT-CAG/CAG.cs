d3960dcf41d0e48683e510d42369b0d56dba511b
eukyre <130890675+eukyre@users.noreply.github.com>
2026-09-25 20:46:47 +0100

    All Rigs + Plates

--- WTT-CAG/CAG.cs
+++ WTT-CAG/CAG.cs
@@ -23,9 +23,9 @@ public record ModMetadata : AbstractModMetadata
     public override List<string>? Incompatibilities { get; init; }
     public override Dictionary<string, Range>? ModDependencies { get; init; } = new()
     {
-        { "com.wtt.commonlib", new Range("~2.0.22") },
+        { "com.wtt.commonlib", new Range("^2.0.22") },
         { "com.wtt.contentbackport", new Range("^1.1.0") },
-        { "com.wtt.armory", new Range("~2.1.0") }
+        { "com.wtt.armory", new Range("^2.1.0") }
     };
     public override string? Url { get; init; }
     public override bool? IsBundleMod { get; init; } = true;
